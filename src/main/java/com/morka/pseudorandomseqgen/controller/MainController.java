57905021a96e889ec39e8358bb1ff6670fc4854f
Mark Putyato <mark_putyato@mail.ru>
2022-09-10 11:45:08 +0300

    Added basic layout with chart and controls on the right

--- src/main/java/com/morka/pseudorandomseqgen/controller/MainController.java
+++ src/main/java/com/morka/pseudorandomseqgen/controller/MainController.java
@@ -2,21 +2,11 @@ package com.morka.pseudorandomseqgen;
 
 import com.morka.pseudorandomseqgen.service.LemerPseudoRandomSequenceGenerator;
 import com.morka.pseudorandomseqgen.service.PseudoRandomSequenceGenerator;
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
-
     private final PseudoRandomSequenceGenerator sequenceGenerator = new LemerPseudoRandomSequenceGenerator.Builder()
             .mod(5)
             .coefficient(3)
             .seed(1)
             .build();
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText(String.valueOf(sequenceGenerator.getNext()));
-    }
 }
